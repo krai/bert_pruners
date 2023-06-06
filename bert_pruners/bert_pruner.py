f18df5335dc17eb8c3fda5c163f3f24070805749
xihajun <www.junfan.com@gmail.com>
2023-06-06 16:59:07 +0000

    update bert_pruners for magnitude pruning

--- bert_pruners/bert_pruner.py
+++ bert_pruners/bert_pruner.py
@@ -1,20 +1,31 @@
 from transformers import BertModel
 from torch.nn.utils import prune
+import torch
 
 class BertPruner:
-    def __init__(self, saved_dir: str, sparsity: float):
-        self.model = BertModel.from_pretrained('bert-base-uncased')
+    def __init__(self, model_name: str, saved_dir: str, sparsity: float):
+        self.model = BertModel.from_pretrained(model_name)
         self.saved_dir = saved_dir
         self.sparsity = sparsity
-
     def prune_and_save(self):
         for name, module in self.model.named_modules():
             if isinstance(module, torch.nn.Linear):
-                prune.l1_unstructured(module, name='weight', amount=self.sparsity)
+                # replace l1_unstructured pruning with l2structured for magnitude pruning
+                # prune.l1_unstructured(module, name='weight', amount=self.sparsity)
+                prune.ln_structured(module, name='weight', amount=self.sparsity, n=2, dim=0)
 
-        torch.onnx.export(self.model,               # model being run
-                          torch.randn(1, 512),      # model input (or a tuple for multiple inputs)
-                          self.saved_dir,           # where to save the model
-                          export_params=True,       # store the trained parameter weights inside the model file
-                          opset_version=10,         # the ONNX version to export the model to
-                          do_constant_folding=True) # whether to execute constant folding for optimization
+        # it fixed using previous code (2 potential changes: eval model/input_id)
+        input_shape = (1, 512)
+        input_ids = torch.zeros(input_shape, dtype=torch.long, device=self.model.device)
+        attention_mask = torch.ones(input_shape, dtype=torch.long, device=self.model.device)
+        token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=self.model.device)
+
+        torch.onnx.export(self.model, (input_ids, attention_mask, token_type_ids),
+                          self.saved_dir,
+                          opset_version=11,
+                          input_names=['input_ids', 'input_mask', 'segment_ids'],
+                          output_names=['output'],
+                          dynamic_axes={'input_ids': {0: 'batch_size', 1: 'sequence_length'},
+                                        'input_mask': {0: 'batch_size', 1: 'sequence_length'},
+                                        'segment_ids': {0: 'batch_size', 1: 'sequence_length'},
+                                        'output': {0: 'batch_size', 1: 'sequence_length'}})
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bert_pruners",
-    version="0.0.1",
+    version="0.0.2",
     author="xihajun",
     author_email="junfan@krai.ai",
     description="Pruning BERT models",
@@ -17,6 +17,7 @@ setup(
     install_requires=[
         'torch',
         'transformers',
+        'onnx'
     ],
     entry_points={
         'console_scripts': [
